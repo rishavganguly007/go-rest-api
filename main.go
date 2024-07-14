112251c748d62dee14ffae04e0d0e43b03874dd3
rishavganguly007 <rishavganguly007@gmail.com>
2024-07-14 22:19:01 +0530

    added models get /event and post /event

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+
+	"example.com/go-rest-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	server := gin.Default() // configures a http server
 	
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.Run(":8080") //localhost port
+
+}
+
+func createEvent(context *gin.Context){
+	var event models.Event
+	context.ShouldBind(&event)
+}
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
 }
